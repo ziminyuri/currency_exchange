a9e9887efdfc1c905f21bb3b319dab89dbd0474a
ziminyuri <zimin.yuri22@gmail.com>
2021-07-22 21:07:22 +0700

    почти доделал конвертирование

--- currency/lib/currency_web/controllers/page_controller.ex
+++ currency/lib/currency_web/controllers/page_controller.ex
@@ -1,22 +1,34 @@
 defmodule CurrencyWeb.PageController do
   use CurrencyWeb, :controller
 
-  alias Currency.{Daily, Repo, Cache, Transform}
+  alias Currency.{Daily, Repo, Cache}
 
   def index(conn, _params) do
-    render conn, "index.html", currencies: Cache.all()
+    table = Cache.all()
+    render conn, "index.html", currencies: table
   end
 
   def converter(conn, _params) do
-    render(conn, "converter.html")
+    table = Cache.all()
+    render conn, "converter.html", currencies: table
   end
 
   def converter_post(conn, params) do
     IO.puts("-----------post--------------")
-    whom = params["email"]["whom"]
-    IO.inspect whom
+    currency_code = params["converter_form"]["currency_code"]
+    sum = params["converter_form"]["sum"]
+
+    currency_data =  currency_code |> Cache.lookup |> List.first
+    currency_value = elem(currency_data, 2)
+    sum1 = String.to_float("12")
+
+    IO.inspect sum1
+    IO.inspect currency_code
+    IO.inspect currency_value
     IO.puts("-----------post--------------")
-    render(conn, "converter.html")
+    table = Cache.all()
+    render conn, "converter.html", currencies: table
+
   end
 
 end
