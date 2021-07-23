6e625b90c0b0284ed2f3180f53eefeaeba1b83bc
ziminyuri <zimin.yuri22@gmail.com>
2021-07-23 15:58:42 +0700

    add test with post request

--- currency/test/currency_web/controllers/page_controller_test.exs
+++ currency/test/currency_web/controllers/page_controller_test.exs
@@ -35,4 +35,36 @@ defmodule CurrencyWeb.PageControllerTest do
     assert tuple_size(data_from_cache) == 3
   end
 
+  test "cache has info about EUR abd USD" do
+    data_from_cache = Currency.Cache.all()
+    assert length(data_from_cache) == 2
+
+    eur_data = List.first(data_from_cache)
+    assert elem(eur_data, 0) == "EUR"
+    assert elem(eur_data, 1) == "Евро"
+    assert tuple_size(eur_data) == 3
+  end
+
+
+  test "converter post request with float", %{conn: conn} do
+    conn = post(conn, Routes.page_path(conn, :converter_post, [converter_form: [currency_code: "USD", input_value_before_convert: "123.4"]]))
+    data_from_cache = Currency.Cache.lookup("USD") |> List.first
+    value_after_convert = elem(data_from_cache, 2) * 123.4
+    assert html_response(conn, 200) =~ "#{value_after_convert}"
+  end
+
+  test "converter post request with int", %{conn: conn} do
+    conn = post(conn, Routes.page_path(conn, :converter_post, [converter_form: [currency_code: "USD", input_value_before_convert: "123"]]))
+    data_from_cache = Currency.Cache.lookup("USD") |> List.first
+    value_after_convert = elem(data_from_cache, 2) * 123
+    assert html_response(conn, 200) =~ "#{value_after_convert}"
+  end
+
+  test "converter post request with negative value", %{conn: conn} do
+    conn = post(conn, Routes.page_path(conn, :converter_post, [converter_form: [currency_code: "USD", input_value_before_convert: "-123"]]))
+    data_from_cache = Currency.Cache.lookup("USD") |> List.first
+    value_after_convert = elem(data_from_cache, 2) * -123
+    assert html_response(conn, 200) =~ "#{value_after_convert}"
+  end
+
 end
